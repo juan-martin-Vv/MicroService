46baa1006d84f99e0c99af506ee9f47e35e16d13
juan-martin-Vv <juan.martin.w@gmail.com>
2022-10-28 15:21:45 -0300

    Eureka Server On!!

--- firstservice/src/main/java/com/jmvv/firstservice/FirstApp.java
+++ firstservice/src/main/java/com/jmvv/firstservice/FirstApp.java
@@ -2,6 +2,7 @@ package com.jmvv.firstservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
 @RequestMapping("/first")
 public class FirstApp 
